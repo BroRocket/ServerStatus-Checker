c064fcab9380fdded49d777d08503d4cb5fefcfc
Brody Howard <95459456+BroRocket@users.noreply.github.com>
2024-07-02 10:06:23 -0400

    Update ServerChecker.py

--- ServerChecker.py
+++ ServerChecker.py
@@ -1,12 +1,19 @@
+#Import necessary libraries 
 import requests
 from tkinter import messagebox
 
-url = "" #Enter server URL here
+#Set url for server
+url = "" #Enter URL here
 
+#Disbales the console warning about sending “Unverified HTTPS request”
 requests.packages.urllib3.disable_warnings()
+#Use try and except in case the request fails
 try:
+	#Makes a web request to the server and grabs status code
     statusCode = requests.head(url, verify=False).status_code
-    messagebox.showinfo("Server Status", f'The status of the Server hosted at {url} is {statusCode}.')
+    #Creates message box on screen and displays the status code of the server
+    messagebox.showinfo("Service Status", f'The status of the Service hosted at {url} is {statusCode}.')
       
 except requests.ConnectionError:
-    messagebox.showinfo("Server Status", f'The program was unable to connect to: {url}')
+    #Creates message box if request failed
+    messagebox.showinfo("Service Status", f'The program was unable to connect to: {url}')
